dc370231b5ed81bcb36b034829d73ca3c10ca9a4
Lee Benson <lee@leebenson.com>
2015-02-11 12:54:52 +0000

    Resolved dependency issues with Connect 3.x

--- package.js
+++ package.js
@@ -1,52 +1,54 @@
 Package.describe({
-  name: 'lee:react',
-  summary: 'client and server side rendering/routing powered by React',
-  version: '0.1.0',
-  git: 'https://github.com/leebenson/react.git'
+    name: 'lee:react',
+    summary: 'client and server side rendering/routing powered by React',
+    version: '0.1.0',
+    git: 'https://github.com/leebenson/react.git'
 });
 
 Npm.depends({
-  'connect': '3.3.4',
-  'connect-route': '0.1.4'
+    'connect': '2.13.0',
+    'connect-route': '0.1.4'
 });
 
 Package._transitional_registerBuildPlugin({
-  name: 'compileJSX',
-  use: [],
-  sources: [
-    'plugin/compile-jsx.js'
-  ],
-  npmDependencies: {
-    'react-tools': '0.12.2'
-  }
+    name: 'compileJSX',
+    use: [],
+    sources: [
+        'plugin/compile-jsx.js'
+    ],
+    npmDependencies: {
+        'react-tools': '0.12.2'
+    }
 });
 
-Package.on_use(function (api) {
-  api.versionsFrom('METEOR@1.0.3.1');
+Package.on_use(function(api) {
+    api.versionsFrom('METEOR@0.9.0');
 
-  api.use(['deps', 'tmeasday:page-js-ie-support@1.3.5'], 'client');
-  api.use('tmeasday:html5-history-api@4.1.2', 'client', {weak: true});
-  api.use('webapp', 'server');
-  api.use('meteorhacks:fast-render@2.3.1', ['client', 'server']);
-  api.use('iron:dynamic-template@1.0.7', 'client');
+    api.use(['deps', 'tmeasday:page-js-ie-support@1.3.5'], 'client');
+    api.use('tmeasday:html5-history-api@4.1.2', 'client', {
+        weak: true
+    });
+    api.use('webapp', 'server');
+    api.use('meteorhacks:fast-render@1.0.0', ['client', 'server']);
+    api.use('iron:dynamic-template@0.3.0', 'client');
 
-  api.add_files('react-with-addons-0.12.2.js', ['client', 'server']);
+    api.add_files('react-with-addons-0.12.2.js', ['client', 'server']);
 
-  api.add_files('routecore-common.js', ['client', 'server']);
-  api.add_files('context-client.js', 'client');
-  api.add_files('context-server.js', 'server');
-  api.add_files('routecore-client.js', 'client');
-  api.add_files('routecore-server.js', 'server');
+    api.add_files('routecore-common.js', ['client', 'server']);
+    api.add_files('context-client.js', 'client');
+    api.add_files('context-server.js', 'server');
+    api.add_files('routecore-client.js', 'client');
+    api.add_files('routecore-server.js', 'server');
 
-  api.add_files('blaze-component.js', ['client', 'server']);
-  api.add_files('reactivity.js', ['client', 'server']);
+    api.add_files('blaze-component.js', ['client', 'server']);
+    api.add_files('reactivity.js', ['client', 'server']);
 
-  api.add_files('body.html', ['client']);
+    api.add_files('body.html', ['client']);
 
-  // fast-render needs to be visible to the app, as the
-  // inline javascript which fast-render pushes to the client
-  // needs to interact with an exported value.
-  api.imply('meteorhacks:fast-render@2.3.1', ['client', 'server']);
-  api.export('RouteCore', ['client', 'server']);
-  // api.export('React'); -- unnecessary (react mutates global obj)
+    // fast-render needs to be visible to the app, as the
+    // inline javascript which fast-render pushes to the client
+    // needs to interact with an exported value.
+    api.imply('meteorhacks:fast-render@1.0.0', ['client', 'server']);
+    api.export('RouteCore', ['client', 'server']);
+    // api.export('React'); -- unnecessary (react mutates global obj)
 });
