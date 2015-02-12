77a40c6609cee4a922f56a90e4e3c89972fe3b28
Lee Benson <lee@leebenson.com>
2015-02-12 00:09:53 +0000

    Removed broken mergeProps

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'lee:react',
     summary: 'client and server side rendering/routing powered by React',
-    version: '0.1.5',
+    version: '0.1.6',
     git: 'https://github.com/leebenson/react.git'
 });
 
--- reactivity.js
+++ reactivity.js
@@ -69,21 +69,3 @@ function createClass(spec) {
 RouteCore.createClass = createClass;
 // XXX: Should React.createClass be overridden?
 React.createClass = createClass;
-
-// Add a mergeProps function
-React.mergeProps = function(props, newProps) {
-    for (var thisKey in newProps) {
-        if (!newProps.hasOwnProperty(thisKey)) {
-            continue;
-        }
-
-        var transferStrategy = TransferStrategies[thisKey];
-
-        if (transferStrategy && TransferStrategies.hasOwnProperty(thisKey)) {
-            transferStrategy(props, thisKey, newProps[thisKey]);
-        } else if (!props.hasOwnProperty(thisKey)) {
-            props[thisKey] = newProps[thisKey];
-        }
-    }
-    return props;
-}
